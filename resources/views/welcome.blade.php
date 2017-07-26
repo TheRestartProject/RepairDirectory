57ad29294d083bdaa98cf0cee92006691713d4dd
Matt Kendon <matt@outlandish.com>
2017-07-26 12:04:15 +0100

    change to lang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{  __('auth.failed')  }}
+                    @lang('auth.failed')
                 </div>
 
                 <div class="links">
