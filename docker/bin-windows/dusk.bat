447d9650a2dc3159fb08a4ecb7e717bc7c0995b8
Matt Kendon <matt@outlandish.com>
2017-07-29 23:24:15 +0100

    updated windows-bin (still needs work). removed dusk.env file

--- docker/bin-windows/dusk.bat
+++
@@ -1,6 +0,0 @@
-
-cd docker
-docker-compose -f docker-compose.yml -f docker-compose.dusk.yml run --rm dusk
-docker-compose -f docker-compose.yml -f docker-compose.dusk.yml stop selenium
-cd ..
-docker/bin/dev start
