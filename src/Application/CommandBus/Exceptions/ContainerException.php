0005f46a2ca149cc62e45f41c620a2905f9e73e7
Tech Circle <tech@outlandish.com>
2017-08-03 20:39:56 +0100

    added test class for laravel container implemented full behaviour added two new exceptions to implement ContainerInterface behaviour

--- src/Application/CommandBus/Exceptions/ContainerException.php
+++ src/Application/CommandBus/Exceptions/ContainerException.php
@@ -4,6 +4,18 @@ namespace TheRestartProject\RepairDirectory\Application\CommandBus\Exceptions;
 
 use Psr\Container\ContainerExceptionInterface;
 
+/**
+ * Implementation of the ContainerExceptionInterface
+ *
+ * This exception implements the ContainerExceptionInterface, part of the ContainerInterface
+ * functionality
+ *
+ * @category Exception
+ * @package  TheRestartProject\RepairDirectory\Application\CommandBus\Exceptions
+ * @author   Matthew Kendon <matt@outlandish.com>
+ * @license  GPLv2 https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ * @link     http://tactician.thephpleague.com/
+ */
 class ContainerException extends \Exception implements ContainerExceptionInterface
 {
 
