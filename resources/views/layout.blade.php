774fdd8e040c530ed0ed645156ef1ad7089c53fb
Matthew Kendon <matt@outlandish.com>
2017-08-09 17:57:33 +0100

    move assets to sub folder to make deployment easier

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"-->
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
-    <script type="text/javascript" src="/js/app.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/map/app.css">
+    <script type="text/javascript" src="/js/map/app.js"></script>
 
     <title>Repair Directory</title>
 </head>
