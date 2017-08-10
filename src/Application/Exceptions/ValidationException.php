6c0b609c590378214f1a5f8652274cb5236f3c84
Joaquim d'Souza <joaquimds@gmail.com>
2017-08-10 17:57:43 +0100

    adds validation

---
+++ src/Application/Exceptions/ValidationException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TheRestartProject\RepairDirectory\Domain\Exceptions;
+
+/**
+ * Class EntityNotFoundException
+ *
+ * @category Exception
+ * @package  TheRestartProject\RepairDirectory\Domain\Exceptions
+ * @author   Joaquim d'Souza <joaquim@outlandish.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.outlandish.com/
+ */
+class EntityNotFoundException extends \Exception
+{
+    protected $message = 'Entity not found';
+    protected $code = 404;
+}
---
+++ tests/Unit/Domain/BusinessValidatorTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheRestartProject\RepairDirectory\Tests\Unit\Domain;
+
+use TheRestartProject\RepairDirectory\Tests\TestCase;
+
+class BusinessValidatorTest extends TestCase {
+    
+}
