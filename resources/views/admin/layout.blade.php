71fb6e188eba60e7dce5e3e18e7fb2af3d8fb033
Matthew Kendon <matt@outlandish.com>
2017-08-11 14:11:49 +0100

    fix link to css

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/map/app.css">
     <title>Repair Directory Admin</title>
 </head>
 <body>
