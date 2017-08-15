e662e0eb6ad36a53181f2a4905f52e6f6b03e580
Joaquim d'Souza <joaquimds@gmail.com>
2017-08-15 11:49:46 +0100

    first pass implementing designs

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -2,48 +2,54 @@
 
 @section('content')
 
-    <form id="search">
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <div class="form-group">
-                    <label for="location">Postcode</label>
-                    <input id="location" name="location" class="form-control">
+    <div class="row no-gutter">
+        <div class="col-xs-12 col-sm-3 sidebar">
+            <div class="sidebar__logo">
+                <img src="/images/logo.png">
+            </div>
+            <div class="sidebar__content">
+                <h3 class="sidebar__header">Restart repair directory</h3>
+                <p class="sidebar__copy">Find a local business to repair your stuff.</p>
+                <form id="search">
+                    <div class="row no-gutter">
+                        <div class="col-xs-12">
+                            <div class="form-group">
+                                <label for="location">Postcode</label>
+                                <input id="location" name="location" class="form-control sidebar__input">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row no-gutter">
+                        <div class="col-xs-12">
+                            <div class="form-group">
+                                <label for="category">Category</label>
+                                <select id="category" name="category" class="form-control sidebar__select">
+                                    <option value="" selected>All</option>
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category }}">
+                                            {{ $category }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row no-gutter">
+                        <div class="col-xs-12">
+                            <div class="form-group">
+                                <button class="btn btn-primary sidebar__button">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="row no-gutter">
+                    <div id="business-details" class="col-xs-12 hidden"></div>
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-6">
-                <div class="form-group">
-                    <label for="category">Category</label>
-                    <select id="category" name="category" class="form-control">
-                        <option value="" selected>All</option>
-                        @foreach($categories as $category)
-                            <option value="{{ $category }}">
-                                {{ $category }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
         </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="form-group">
-                    <button class="btn btn-primary form-group">Search</button>
-                </div>
-            </div>
+        <div class="col-xs-12 col-sm-9 map">
+            <div id="map"></div>
         </div>
-    </form>
-    <div class="row">
-        <div class="col-xs-12 col-md-8">
-            <div class="map__container">
-                <div id="map"></div>
-            </div>
-        </div>
-        <div id="business-details" class="col-xs-12 col-md-4 hidden"></div>
-        <div id="business-details-placeholder" class="col-xs-12 col-md-4">
-            <h2>All repairers</h2>
-            <p>Select an individual repairer to view details</p>
-        </div>
-
     </div>
 
     <script async defer onload="initMap()"
