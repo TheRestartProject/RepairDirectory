2328e209dd03452e6e9315e4a533985e13e89006
Paul Fauth-Mayer <paul@outlandish.com>
2017-08-16 12:19:31 +0100

    logs again

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -3,11 +3,9 @@ var $slider = $("#positiveReviewPcRange");
 var $sliderValue = $("#positiveReviewPc");
 
 $slider.change(function () {
-    console.log("slider.change")
     $sliderValue.val($slider.val());
 });
 
 $sliderValue.blur(function () {
-    console.log("sliderValue.blur")
     $slider.val($sliderValue.val());
 });
