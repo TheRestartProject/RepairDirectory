2a639145bcdde6aaec0c25e635a56054f0d1c5c0
Matthew Kendon <matt@outlandish.com>
2017-08-19 12:12:32 +0100

    add route for logging in

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -64,6 +64,7 @@ class RouteServiceProvider extends ServiceProvider
                 Route::namespace('Auth')
                     ->group(function () {
                         Route::get('/login', 'LoginController@showLoginForm')->name('login');
+                        Route::post('/login', 'LoginController@login');
                     });
 
                 Route::prefix('map')
