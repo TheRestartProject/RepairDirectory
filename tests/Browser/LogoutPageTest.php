f6c77861b6dce34c810fa24f40155d80a8151269
Matthew Kendon <matt@outlandish.com>
2017-08-19 19:48:55 +0100

    added logout page test

--- tests/Browser/LogoutPageTest.php
+++ tests/Browser/LogoutPageTest.php
@@ -14,6 +14,19 @@ class LogoutPageTest extends DuskTestCase
     /**
      * @test
      */
+    public function i_get_redirected_if_i_try_to_logout_when_not_logged_in()
+    {
+        /** @var User $user */
+        $user = entity(User::class)->create();
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit(route('logout'))
+                ->assertRouteIs('map');
+        });
+    }
+
+    /**
+     * @test
+     */
     public function i_can_logout_if_i_am_logged_in()
     {
         /** @var User $user */
