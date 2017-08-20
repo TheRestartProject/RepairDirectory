6314f44e660d310fc804f14c396d0a467f827ad4
Matthew Kendon <matt@outlandish.com>
2017-08-20 19:09:04 +0100

    added FixometerSession

---
+++ src/Domain/Models/FixometerSession.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matt
+ * Date: 20/08/17
+ * Time: 19:07
+ */
+
+namespace TheRestartProject\RepairDirectory\Domain\Models;
+
+
+class FixometerSession
+{
+    private $sessionid;
+
+    private $session;
+
+    private $user;
+}
