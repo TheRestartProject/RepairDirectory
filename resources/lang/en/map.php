6e6b8d1ba529afc11a1c1187eb1af3143802f6b9
Matthew Kendon <matt@outlandish.com>
2017-08-25 17:11:08 +0100

    added language stuff for the map

---
+++ resources/lang/en/map.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Repair Directory',
+    'location' => 'Your location',
+    'category' => 'Category',
+    'category_all' => 'All',
+    'search' => 'Search',
+    'header_copy' => 'Find a local business to repair your stuff.',
+    'header_title' => 'Restart repair directory'
+];
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="/css/map/app.css">
     <script type="text/javascript" src="/js/map/app.js"></script>
 
-    <title>Repair Directory</title>
+    <title>{{ __('map.title') }}</title>
 </head>
 <body>
 <div>
