8689b55374f7e175e7e723df36f6404c38a59c71
Matthew Kendon <matt@outlandish.com>
2017-08-25 17:06:11 +0100

    added language stuff for the admin section

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 
-    <h2>All Repairers</h2>
+    <h2>{{ __('admin.form_title') }}</h2>
     <a class="btn btn-primary" href="{{ route('admin.business.edit') }}">Add repairer</a>
 
     <table class="table table-striped table-hover">
         <thead>
         <tr>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Postcode</th>
-            <th>Description</th>
+            <th>{{ __('admin.name') }}</th>
+            <th>{{ __('admin.address') }}</th>
+            <th>{{ __('admin.postcode') }}</th>
+            <th>{{ __('admin.categories') }}</th>
+            <th>{{ __('admin.average_scores') }}</th>
+            <th>{{ __('admin.publishing_status') }}</th>
         </tr>
         </thead>
         <tbody>
@@ -21,7 +23,9 @@
                 <td>{{ $business->getName() }}</td>
                 <td>{{ $business->getAddress() }}</td>
                 <td>{{ $business->getPostcode() }}</td>
-                <td>{{ $business->getDescription() }}</td>
+                <td>{{ implode(', ', $business->getCategories()) }}</td>
+                <td>{{ $business->getAverageScore() }}</td>
+                <td>{{ $business->getPublishingStatus() }}</td>
             </tr>
         @endforeach
         </tbody>
