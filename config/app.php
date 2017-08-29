f892f369735798c3b7b3772256f37a3a3451aae5
Joaquim d'Souza <joaquimds@gmail.com>
2017-08-29 11:52:15 +0100

    partially rebased and fixed

--- config/app.php
+++ config/app.php
@@ -184,7 +184,6 @@ return [
         App\Providers\BusinessServiceProvider::class,
         App\Providers\GeocoderServiceProvider::class,
         App\Providers\SuggestionServiceProvider::class,
-        App\Providers\LocationServiceProvider::class,
         App\Providers\PhantomServiceProvider::class,
         App\Providers\FixometerServiceProvider::class
 
