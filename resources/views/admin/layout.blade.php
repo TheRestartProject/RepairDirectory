7c141f057d7b34e366a83f30f844ca0180cca628
Neil Mather <ngmather@gmail.com>
2018-02-02 11:56:46 +0000

    Small tweaks to branding

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -10,14 +10,29 @@
     <title>@lang('admin.title')</title>
 </head>
 <body>
-<div class="container">
-    <a href='/map' class="header__link btn btn-primary">@lang('admin.go_to_map')</a>
-    <h1><a href="{{ route('admin.index') }}">@lang('admin.title')</a></h1>
-    @yield('content')
+
+    <header>
+        <div class="container">
+            <a href='/map' class="header__link btn btn-primary">@lang('admin.go_to_map')</a>
+            <h1><a href="{{ route('admin.index') }}">@lang('admin.title')</a></h1>
+        </div>
+    </header>
+
+    <main role="main">
+        <div class="container">
+            @yield('content')
+        </div>
+    </main>
+
+    <footer class="text-muted">
+        <div class="container">
+            <p>Created by the <a href="https://therestartproject.org">The Restart Project</a> and <a href="https://outlandish.com">Outlandish</a>.</p>
+        </div>
+    </footer>
+
     <script src="/js/map/admin.js"></script>
     <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
     @stack('scripts')
-</div>
 </body>
 </html>
