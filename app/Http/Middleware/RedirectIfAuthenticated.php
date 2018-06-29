0c44d35ec94a7c6ffa179bdd07c4a73c0f8b217a
Neil Mather <ngmather@gmail.com>
2018-06-29 17:48:08 +0100

    Remove the ability to access a local login screen

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,9 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        //if (Auth::guard($guard)->check()) {
-        //   return redirect('/map');
-        //}
+        if (Auth::guard($guard)->check()) {
+           return redirect('/');
+        }
 
         return $next($request);
     }
--- routes/local.php
+++ routes/local.php
@@ -2,15 +2,15 @@
 
 use TheRestartProject\Fixometer\Domain\Repositories\UserRepository;
 
-Route::get('/', function (UserRepository $repository) {
+/*Route::get('/', function (UserRepository $repository) {
     $users = $repository->findAll();
     $loggedInUser = Auth::user();
     return view('home', compact('users', 'loggedInUser'));
-})->name('home');
+    })->name('home');*/
 
-Route::namespace('Auth')
+/*Route::namespace('Auth')
     ->group(function () {
         Route::get('/login', 'LoginController@showLoginForm')->name('login');
         Route::post('/login', 'LoginController@login');
         Route::get('/logout', 'LoginController@logout')->name('logout');
-    });
+        });*/
