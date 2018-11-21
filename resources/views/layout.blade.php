4a4ed4a8a5f566bdd9179e1529de5d20b0cbb8de
Neil Mather <ngmather@gmail.com>
2018-11-21 21:54:29 +0000

    Add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
     <!--link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/map/app.css">
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
 
     <script>
         window.__env = {
