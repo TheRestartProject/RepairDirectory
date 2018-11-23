35a6797993843a4c9ad988156b82dec6bf498ba8
Neil Mather <ngmather@gmail.com>
2018-11-23 16:10:58 +0000

    Manually upgrade CreatesApplication.php for Laravel 5.6

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,17 +2,9 @@
 
 namespace TheRestartProject\RepairDirectory\Tests;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Contracts\Console\Kernel;
 
-/**
- * Trait CreatesApplication
- *
- * @category Trait
- * @package  TheRestartProject\RepairDirectory\Tests
- * @author   Matt Kendon <matt@outlandish.com>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://www.outlandish.com/
- */
 trait CreatesApplication
 {
     /**
@@ -22,9 +14,11 @@ trait CreatesApplication
      */
     public function createApplication()
     {
-        $app = include __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+
+        Hash::driver('bcrypt')->setRounds(4);
 
         return $app;
     }
