6ea21fa01cb93957c9ef34abc880978694cf772e
Edward Hibbert <edward@ehibbert.org.uk>
2021-11-23 14:26:34 +0000

    New migration needs adjusting for shift.

--- database/migrations/Version20211110153738.php
+++ database/migrations/Version20211110153738.php
@@ -10,7 +10,7 @@ class Version20211110153738 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSQL("ALTER TABLE `businesses` CHANGE `hide_reason` `hide_reason` ENUM('Closed temporarily','Closed permanently','Doesn''t meet quality criteria','Asked to be removed','Other','Not in communication') CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;");
@@ -19,7 +19,7 @@ class Version20211110153738 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSQL("ALTER TABLE `businesses` CHANGE `hide_reason` `hide_reason` ENUM('Closed temporarily','Closed permanently','Doesn''t meet quality criteria','Asked to be removed','Other') CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;;");
