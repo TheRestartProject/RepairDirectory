fb658f0e8b7ae58aae73afcfa78c928ce419cc73
Neil Mather <ngmather@gmail.com>
2022-06-06 17:51:23 +0100

    Update reuse UK region

--- src/Domain/Enums/Region.php
+++ src/Domain/Enums/Region.php
@@ -9,7 +9,7 @@ class Region extends Enum
 {
     const LONDON = 'London';
     const WALES = 'Wales';
-    const UK = 'UK';
+    const REUSE_UK = 'UK';
     
     const CATEGORIES = [
         self::LONDON => [
@@ -97,42 +97,19 @@ class Region extends Enum
             Category::BICYCLE_BIKE_CYCLE,
             Category::FURNITURE_UPHOLSTERY,
         ],
-        self::UK => [
+        self::REUSE_UK => [
             Category::APPLE_IPHONE,
             Category::APPLE_IPAD,
-            Category::AIRCON_DEHUMIDIFIER,
-            Category::DECORATIVE_LIGHTS,
             Category::DESKTOP,
-            Category::CAMERA,
-            Category::VIDEO_CAMERA,
-            Category::FAN,
             Category::FLAT_SCREEN,
-            Category::HAIR_AND_BEAUTY,
-            Category::HANDHELD_ENTERTAINMENT,
-            Category::HEADPHONES,
-            Category::HIFI,
-            Category::KETTLE,
-            Category::LAMP,
             Category::LAPTOP,
             Category::MOBILE,
-            Category::INSTRUMENT,
             Category::NINTENDO_CONSOLE,
-            Category::SHREDDER,
             Category::PC_ACCESSORY,
             Category::PLAYSTATION_CONSOLE,
-            Category::PORTABLE_RADIO,
-            Category::POWER_TOOL,
             Category::PRINTER_SCANNER,
-            Category::PROJECTOR,
-            Category::SEWING_MACHINE,
-            Category::SMALL_KITCHEN_ITEM,
             Category::TABLET,
-            Category::TOASTER,
-            Category::TOY,
-            Category::TV,
-            Category::VACUUM,
             Category::XBOX_CONSOLE,
-            Category::WHITE_GOODS
         ],
     ];
 
@@ -183,7 +160,7 @@ class Region extends Enum
                     'value' => PublishingStatus::PUBLISHED
                 ]
             ],
-        self::UK =>
+        self::REUSE_UK =>
             [
                 [
                     'field' => 'address',
