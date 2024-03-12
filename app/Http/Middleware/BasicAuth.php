4e9f7d670c69612d7b417dd93275264a3c09220d
Edward Hibbert <edward@ehibbert.org.uk>
2024-03-12 13:06:41 +0000

    WIP Platform.sh

--- app/Http/Middleware/BasicAuth.php
+++ app/Http/Middleware/BasicAuth.php
@@ -16,16 +16,19 @@ class BasicAuth
         $AUTH_PASS = config('auth.BASIC_AUTH_PASS');
         header('Cache-Control: no-cache, must-revalidate, max-age=0');
         $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+        echo $_SERVER['PHP_AUTH_USER'] . " vs " . $AUTH_USER . "\n";
+        echo $_SERVER['PHP_AUTH_PW'] . " vs " . $AUTH_PASS . "\n";
         $is_not_authenticated = (
             !$has_supplied_credentials ||
             $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
             $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
         );
-        if ($is_not_authenticated) {
-            header('HTTP/1.1 401 Authorization Required');
-            header('WWW-Authenticate: Basic realm="Access denied"');
-            exit;
-        }
+        echo "Not auth " . $is_not_authenticated . "\n";
+//        if ($is_not_authenticated) {
+//            header('HTTP/1.1 401 Authorization Required');
+//            header('WWW-Authenticate: Basic realm="Access denied"');
+//            exit;
+//        }
         return $next($request);
     }
 }
