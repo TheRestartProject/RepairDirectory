24c611ea5e70eb5e95493d5685fbbb80b4f1c27a
Graham <janus-git@exmosis.net>
2024-03-28 15:35:23 +0000

    REP-267 Refactor previous work and hardcode reuse dir labels, and apply to Warranty fields

--- lang/en/admin.php
+++ lang/en/admin.php
@@ -1,5 +1,20 @@
 <?php
 
+if (env('DIRECTORY_TYPE', 'repair') == 'reuse') {
+    $generic_field1_label = 'Device Acceptance Criteria';
+    $warranty_offered_label = 'Has Data Erasure Policy';
+    $warranty_details_label = 'Data Erasure Policy Details';
+} else {
+    $generic_field1_label = 'Generic Field';
+    $warranty_offered_label = 'Warranty Offered';
+    $warranty_details_label = 'Warranty Details';
+}
+
+// Allow override in .env for now
+$generic_field1_label = env('BUSINESS_FIELD_LABEL_GENERIC_FIELD1', $generic_field1_label);
+$warranty_offered_label = env('BUSINESS_FIELD_LABEL_WARRANTY_OFFERED', $warranty_offered_label);
+$warranty_details_label = env('BUSINESS_FIELD_LABEL_WARRANTY_DETAILS', $warranty_details_label);
+
 return [
 
     'title' => 'Repair Directory Admin',
@@ -25,15 +40,15 @@ return [
     'qualifications' => 'Qualifications',
     'community_endorsement' => 'Community Endorsement',
     'notes' => 'Notes',
-    'generic_field1' => env('BUSINESS_FIELD_LABEL_GENERIC_FIELD1', 'Device acceptance criteria'),
+    'generic_field1' => $generic_field1_label,
     'positive_review_percentage' => 'Positive Review Percentage',
     'average_scores' => 'Average score',
     'review_source' => 'Review Source',
     'number_of_reviews' => 'Number of Reviews',
     'review_count' => 'Review Count',
     'review_percent' => 'Review %',
-    'warranty_offered' => 'Warranty Offered',
-    'warranty_details' => 'Warranty Details',
+    'warranty_offered' => $warranty_offered_label,
+    'warranty_details' => $warranty_details_label,
     'publishing_status' => 'Publishing Status',
     'hide_reason' => 'Hide Reason',
 
