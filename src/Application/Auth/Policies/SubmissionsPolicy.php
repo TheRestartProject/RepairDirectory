5e8bfd8915615dbaf57f6316820992d87dc2501f
Edward Hibbert <edward@ehibbert.org.uk>
2024-04-08 12:52:31 +0100

    WIP Platform.sh

--- src/Application/Auth/Policies/SubmissionsPolicy.php
+++ src/Application/Auth/Policies/SubmissionsPolicy.php
@@ -9,26 +9,27 @@ class SubmissionsPolicy
 {
     public function before(User $user)
     {
-        if ($user->isSuperAdmin($user)) {
+// TODO Using BasicAuth on Platform, so auto-login as SuperAdmin.
+//        if ($user->isSuperAdmin($user)) {
             return true;
-        }
+//        }
     }
 
     public function index(User $user)
     {
-        if ($user->isRegionalAdmin() || $user->isEditor())
+//        if ($user->isRegionalAdmin() || $user->isEditor())
             return true;
     }
 
     public function view(User $user)
     {
-        if ($user->isRegionalAdmin() || $user->isEditor())
+//        if ($user->isRegionalAdmin() || $user->isEditor())
             return true;
     }
 
     public function update(User $user)
     {
-        if ($user->isSuperAdmin() || $user->isRegionalAdmin())
+//        if ($user->isSuperAdmin() || $user->isRegionalAdmin())
             return true;
     }
 }
