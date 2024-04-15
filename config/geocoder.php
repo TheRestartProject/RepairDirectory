018ad1ef29910ab602487ffdae608185987cd093
Edward Hibbert <edward@ehibbert.org.uk>
2024-04-15 11:27:19 +0100

    WIP Platform.sh - environment variables

--- config/geocoder.php
+++ config/geocoder.php
@@ -11,24 +11,12 @@
 
 use Geocoder\Provider\GoogleMaps\GoogleMaps;
 
-$api_key = env('GOOGLE_MAPS_API_KEY');
-
-try {
-    $config = new \Platformsh\ConfigReader\Config();
-
-    if ($config->isValidPlatform()) {
-        $api_key = $config->variable('GOOGLE_MAPS_API_KEY', '');
-    }
-} catch (Exception $e) {
-    # This can happen during build phase.
-}
-
 return [
     'cache-duraction' => 999999999,
     'providers' => [
         GoogleMaps::class => [
             null,
-            $api_key,
+            env('GOOGLE_MAPS_API_KEY'),
         ],
     ]
 ];
