a96ee0362bf65ab616ff553002d338f885e21056
Graham <janus-git@exmosis.net>
2024-04-16 15:33:30 +0100

    REP-267 Clarify that the Generic Field is unused on Repair Dir

--- lang/en/admin.php
+++ lang/en/admin.php
@@ -5,7 +5,7 @@ if (env('DIRECTORY_TYPE', 'repair') == 'reuse') {
     $warranty_offered_label = 'Has Data Erasure Policy';
     $warranty_details_label = 'Data Erasure Policy Details';
 } else {
-    $generic_field1_label = 'Generic Field';
+    $generic_field1_label = 'Generic Field (Unused)';
     $warranty_offered_label = 'Warranty Offered';
     $warranty_details_label = 'Warranty Details';
 }
