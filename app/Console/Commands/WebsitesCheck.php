e9469b69ebbee52e7f4331ad54fa1cdaa662038f
Edward Hibbert <edward@ehibbert.org.uk>
2024-04-29 12:24:11 +0100

    WIP Platform.sh - CheckBusinesses.

--- app/Console/Commands/WebsitesCheck.php
+++ app/Console/Commands/WebsitesCheck.php
@@ -110,7 +110,7 @@ class WebsitesCheck extends Command
             }
         }
 
-        if (count($errors)) {
+//        if (count($errors)) {
             $this->error("Found " . count($errors) . " errors");
 
             // In a Platform environment we will be automatically logged in, and we notify that user rather
@@ -135,6 +135,6 @@ class WebsitesCheck extends Command
                     $admin->notify(new AdminBusinessWebsiteInvalid($errors));
                 }
             }
-        }
+//        }
     }
 }
