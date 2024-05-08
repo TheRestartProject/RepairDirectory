53d713cc39c8f6ce72e020688a0a90d95b5bf7da
Graham <janus-git@exmosis.net>
2024-05-08 14:41:45 +0100

    REP-270 Add font styling to review email

--- resources/views/emails/businesses/details_check.blade.php
+++ resources/views/emails/businesses/details_check.blade.php
@@ -1,19 +1,31 @@
-<h2>Check business details</h2>
+<html>
+<head>
+    <style type="text/css">
+        body > * {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
 
-<ul>
-    <li><strong>{{ __('admin.name') }}:</strong>  {{ $business->getName() }}</li>
-    <li><strong>{{ __('admin.address') }}:</strong> {{ $business->getAddress() }}</li>
-    <li><strong>{{ __('admin.postcode') }}:</strong> {{ $business->getPostcode() }}</li>
-    <li><strong>{{ __('admin.local_area') }}:</strong> {{ $business->getLocalAreaName() }}</li>
-    <li><strong>{{ __('admin.categories') }}:</strong> {{ implode(', ', $business->getCategories()) }}</li>
-    <li><strong>{{ __('admin.review_count') }}:</strong> {{ $business->getNumberOfReviews() }}</li>
-    <li><strong>{{ __('admin.review_percent') }}:</strong> {{ $business->getPositiveReviewPc() }}</li>
-    <li><strong>{{ __('admin.publishing_status') }}:</strong> {{ $business->getPublishingStatus() }}</li>
-    <li><strong>Notes:</strong> {{ $business->getNotes() }}</li>
-    <li><strong>{{ __('admin.hide_reason') }}:</strong> {{ $business->getHideReason() }}</li>
-</ul>
+    <h2>Check business details</h2>
 
-<div>
-    Please reply to this email with any amendments needed.
-</div>
+    <ul>
+        <li><strong>{{ __('admin.name') }}:</strong>  {{ $business->getName() }}</li>
+        <li><strong>{{ __('admin.address') }}:</strong> {{ $business->getAddress() }}</li>
+        <li><strong>{{ __('admin.postcode') }}:</strong> {{ $business->getPostcode() }}</li>
+        <li><strong>{{ __('admin.local_area') }}:</strong> {{ $business->getLocalAreaName() }}</li>
+        <li><strong>{{ __('admin.categories') }}:</strong> {{ implode(', ', $business->getCategories()) }}</li>
+        <li><strong>{{ __('admin.review_count') }}:</strong> {{ $business->getNumberOfReviews() }}</li>
+        <li><strong>{{ __('admin.review_percent') }}:</strong> {{ $business->getPositiveReviewPc() }}</li>
+        <li><strong>{{ __('admin.publishing_status') }}:</strong> {{ $business->getPublishingStatus() }}</li>
+        <li><strong>Notes:</strong> {{ $business->getNotes() }}</li>
+        <li><strong>{{ __('admin.hide_reason') }}:</strong> {{ $business->getHideReason() }}</li>
+    </ul>
 
+    <div>
+        Please reply to this email with any amendments needed.
+    </div>
+
+</body>
+</html>
