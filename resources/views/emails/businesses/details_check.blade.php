306e0f1ec1fa848c1a9e080bfe0fb5928fb3641b
Graham <janus-git@exmosis.net>
2024-05-09 11:08:49 +0100

    REP-270 Update business check mail fields and use different headers for Reuse Dir

--- resources/views/emails/businesses/details_check.blade.php
+++ resources/views/emails/businesses/details_check.blade.php
@@ -16,15 +16,11 @@
         <li><strong>{{ __('admin.postcode') }}:</strong> {{ $business->getPostcode() }}</li>
         <li><strong>{{ __('admin.description') }}:</strong> {{ $business->getDescription() }}</li>
 
-        <li><strong>{{ __('admin.landline') }}:</strong> {{ $business->getLandline() }}</li>
+        <li><strong>{{ __('admin.phone') }}:</strong> {{ $business->getLandline() }}</li>
         <li><strong>{{ __('admin.mobile') }}:</strong> {{ $business->getMobile() }}</li>
         <li><strong>{{ __('admin.website') }}:</strong> {{ $business->getWebsite() }}</li>
         <li><strong>{{ __('admin.email') }}:</strong> {{ $business->getEmail() }}</li>
 
-        <li><strong>{{ __('admin.local_area') }}:</strong> {{ $business->getLocalAreaName() }}</li>
-
-        <li><strong>{{ __('admin.products_repaired') }}:</strong> {{ implode(', ', $business->getProductsRepaired()) }}</li>
-        <li><strong>{{ __('admin.authorised_brands') }}:</strong> {{ implode(', ', $business->getAuthorisedBrands()) }}</li>
         <li><strong>{{ __('admin.categories') }}:</strong> {{ implode(', ', $business->getCategories()) }}</li>
         <li><strong>{{ __('admin.qualifications') }}:</strong> {{ $business->getQualifications() }}</li>
         <li><strong>{{ __('admin.community_endorsement') }}:</strong> {{ $business->getCommunityEndorsement() }}</li>
@@ -35,12 +31,6 @@
         <li><strong>{{ __('admin.warranty_offered') }}:</strong> {{ $business->isWarrantyOffered() }}</li>
         <li><strong>{{ __('admin.warranty_details') }}:</strong> {{ $business->getWarranty() }}</li>
 
-        <li><strong>{{ __('Review Source URL') }}:</strong> {{ $business->getReviewSourceUrl() }}</li>
-        <li><strong>{{ __('admin.review_source') }}:</strong> {{ $business->getReviewSource() }}</li>
-        <li><strong>{{ __('admin.review_count') }}:</strong> {{ $business->getNumberOfReviews() }}</li>
-        <li><strong>{{ __('admin.average_scores') }}:</strong> {{ $business->getAverageScore() }}</li>
-        <li><strong>{{ __('admin.review_percent') }}:</strong> {{ $business->getPositiveReviewPc() }}</li>
-        
     </ul>
 
     <div>
