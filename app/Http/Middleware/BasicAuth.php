81b9b3e6752b71ecf143b84f73305844cbec821d
Edward Hibbert <edward@ehibbert.org.uk>
2024-05-31 13:28:35 +0100

    WIP Upsun - basic auth

--- app/Http/Middleware/BasicAuth.php
+++ app/Http/Middleware/BasicAuth.php
@@ -15,29 +15,27 @@ class BasicAuth
     public function handle($request, Closure $next)
     {
         Log::error("Basic auth handle");
-        header('X-Debug-Upsun-1:', "Basic auth handle");
         $user = null;
         $pass = null;
 
         try {
             $config = new \Platformsh\ConfigReader\Config();
             Log::error("Got config");
-            header('X-Debug-Upsun-2:', "Got config");
 
             if ($config->isValidPlatform()) {
                 Log::error("Valid platform");
-                header('X-Debug-Upsun-3:', "Valid platform");
                 $user = $config->variable('BASIC_AUTH_USER', '');
                 $pass = $config->variable('BASIC_AUTH_PASS', '');
-                header('X-Debug-Upsun-4:', "Auth user " . $user);
             }
         } catch (\Exception $e) {
             Log::error("No basic auth user and pass " . $e->getMessage());
-            header('X-Debug-Upsun-5:', "No basic auth user and pass " . $e->getMessage());
         }
+
+        Log::error("User $user pass $pass");
 
         if ($user && $pass) {
             // Basic auth configured, apply.
+            Log::error("Basic auth configured");
             header('Cache-Control: no-cache, must-revalidate, max-age=0');
             $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
             if ($has_supplied_credentials) {
@@ -45,15 +43,18 @@ class BasicAuth
                 Log::error('X-Auth-Debug2: ' . $_SERVER['PHP_AUTH_PW'] . " vs " . $pass);
             }
 
+            Log::error("Has supplied credentials $has_supplied_credentials, user " . $_SERVER['PHP_AUTH_USER'] . " pass " . $_SERVER['PHP_AUTH_PW']);
             $is_not_authenticated = (
                 !$has_supplied_credentials ||
                 $_SERVER['PHP_AUTH_USER'] != $user ||
                 $_SERVER['PHP_AUTH_PW']   != $pass
             );
 
+            Log::error("Is not authenticated $is_not_authenticated");
             if ($is_not_authenticated) {
                 header('HTTP/1.1 401 Authorization Required');
                 header('WWW-Authenticate: Basic realm="Access denied"');
+                Log::error("Unauthorized");
                 exit;
             }
         }
