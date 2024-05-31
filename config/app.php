f86a2d67fcc418f3df539f8fe97ebdfb08d3b177
Edward Hibbert <edward@ehibbert.org.uk>
2024-05-31 12:53:33 +0100

    Add debug bar.

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
