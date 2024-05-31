992d5c124b91ab17a0dae07ed29a1e8020ac72e0
Edward Hibbert <edward@ehibbert.org.uk>
2024-05-31 13:21:04 +0100

    WIP Upsun - basic auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            'basicAuth' => \App\Http\Middleware\BasicAuth::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -63,6 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'basicAuth' => \App\Http\Middleware\BasicAuth::class,
     ];
 }
