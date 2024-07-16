92b875439fb57d5066526fcf650499ceb2809edb
Graham <janus-git@exmosis.net>
2024-07-16 12:37:28 +0100

    REP-270 Business fields can return hyperlinked fields, and apply to check email

--- resources/views/emails/businesses/details_check.blade.php
+++ resources/views/emails/businesses/details_check.blade.php
@@ -26,23 +26,23 @@
             </p>
 
             <div id="details">
-                <ul>
+                <ul style="line-height: 2em">
                     <li><strong>{{ __('admin.name') }}:</strong>  {{ $business->getName() }}</li>
                     <li><strong>{{ __('admin.address') }}:</strong> {{ $business->getAddress() }}</li>
                     <li><strong>{{ __('admin.postcode') }}:</strong> {{ $business->getPostcode() }}</li>
                     <li><strong>{{ __('admin.description') }}:</strong> {{ $business->getDescription() }}</li>
                     <li><strong>{{ __('admin.phone') }}:</strong> {{ $business->getLandline() }}</li>
                     <li><strong>{{ __('admin.mobile') }}:</strong> {{ $business->getMobile() }}</li>
-                    <li><strong>{{ __('admin.website') }}:</strong> {{ $business->getWebsite() }}</li>
+                    <li><strong>{{ __('admin.website') }}:</strong> {!! $business->getWebsite(true) !!}</li>
                     <li><strong>{{ __('admin.email') }}:</strong> {{ $business->getEmail() }}</li>
                     <li><strong>{{ __('admin.categories') }}:</strong> {{ implode(', ', $business->getCategories()) }}</li>
-                    <li><strong>{{ __('admin.qualifications') }}:</strong> {{ $business->getQualifications() }}</li>
+                    <li><strong>{{ __('admin.qualifications') }}:</strong> {!! $business->getQualifications(true) !!}</li>
                     <li><strong>{{ __('admin.community_endorsement') }}:</strong> {{ $business->getCommunityEndorsement() }}</li>
-                    <li><strong>{{ __('admin.custom_field1') }}:</strong> {{ $business->getCustomField1() }}</li>
-                    <li><strong>{{ __('admin.custom_field2') }}:</strong> {{ $business->getCustomField2() }}</li>
-                    <li><strong>{{ __('admin.custom_field3') }}:</strong> {{ $business->getCustomField3() }}</li>
+                    <li><strong>{{ __('admin.custom_field1') }}:</strong> {!! $business->getCustomField1(true) !!}</li>
+                    <li><strong>{{ __('admin.custom_field2') }}:</strong> {!! $business->getCustomField2(true) !!}</li>
+                    <li><strong>{{ __('admin.custom_field3') }}:</strong> {!! $business->getCustomField3(true) !!}</li>
                     <li><strong>{{ __('admin.warranty_offered') }}:</strong> {{ $business->isWarrantyOffered() }}</li>
-                    <li><strong>{{ __('admin.warranty_details') }}:</strong> {{ $business->getWarranty() }}</li>
+                    <li><strong>{{ __('admin.warranty_details') }}:</strong> {!! $business->getWarranty(true) !!}</li>
                 </ul>
             </div>
 
